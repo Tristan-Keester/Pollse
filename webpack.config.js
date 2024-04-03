9dae6a6f870d022e52c597f4bc126433dc2ffe5a
Tristan Keester <tristankeester@gmail.com>
2024-04-03 14:51:46 -0500

    Fixed react-router on refresh

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,9 @@ module.exports = {
   mode: 'development',
   entry: './client/index.tsx',
   output: {
-    path: path.join(__dirname, "/dist"), // the bundle output path
+    path: path.resolve(__dirname, "dist"), // the bundle output path
     filename: "bundle.js", // the name of the bundle
+    publicPath: '/'
   },
   module: {
     rules: [
@@ -19,11 +20,6 @@ module.exports = {
         test: /\.(sa|sc|c)ss$/, // styles files
         use: ["style-loader", "css-loader", "sass-loader"],
       },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
-        loader: "url-loader",
-        options: { limit: false },
-      },
     ],
   },
   resolve: {
@@ -31,17 +27,10 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "client/index.html", // to import index.html file inside index.ts
+      template: "./client/index.html", // to import index.html file inside index.ts
     }),
   ],
   devServer: {
-    static: {
-      directory: path.join(__dirname, 'build'),
-    },
-    proxy: {
-      '/api/': 'http://localhost:3000',
-    },
-    port: 8080, // you can change the port
-    hot: true,
+    historyApiFallback: true
   },
 };
